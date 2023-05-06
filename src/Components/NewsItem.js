7ebcb60b19746f75fd8ecd8aa04e8d1707d3422b
Ayush Pawar <ayushpawar27@gmail.com>
2023-05-06 11:30:14 +0530

    refactoring news component

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
           <img src={!imageUrl?"https://media.istockphoto.com/id/1177877876/photo/newspaper-and-coffee-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=AkHmICq072B8pT32n4EYjofe5dYqtDKaJ45v3R8qxQU=":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
-              <span class="badge badge text-bg-info ">{source}</span>
+              <span className="badge badge text-bg-info ">{source}</span>
               <p className="card-text">{description}...</p>
               <p className="card-text"><small className="text-body-secondary">By {!author? "Unknown":author} on {new Date(date).toGMTString()}</small></p>
               <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm" rel="noreferrer">Read More</a>
