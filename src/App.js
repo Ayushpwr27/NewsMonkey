6e5fc9a92967563edbb138dfb065addf09755a23
Ayush Pawar <ayushpawar27@gmail.com>
2023-06-10 20:53:34 +0530

    Hiding API Key using .env.local file

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize =5;
+  apiKey = process.env.REACT_APP_NEWS_APP
   state = {
     progress:0
   }
@@ -30,21 +31,21 @@ export default class App extends Component {
         height={3}
       />
         <Routes>
-            <Route path="/home" element={<News setProgress={this.setProgress} key='general' pageSize = {this.pageSize}  country="in" category="general"/>}>
+            <Route path="/home" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='general' pageSize = {this.pageSize}  country="in" category="general"/>}>
             </Route>
-            <Route path="/business" element={<News setProgress={this.setProgress} key='business' pageSize = {this.pageSize}  country="in" category="business"/>}>
+            <Route path="/business" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='business' pageSize = {this.pageSize}  country="in" category="business"/>}>
             </Route>
-            <Route path="/entertainment" element={<News setProgress={this.setProgress} key='entertainment' pageSize = {this.pageSize}  country="in" category="entertainment"/>}>
+            <Route path="/entertainment" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='entertainment' pageSize = {this.pageSize}  country="in" category="entertainment"/>}>
             </Route>
-            <Route path="/general" element={<News setProgress={this.setProgress} key='general' pageSize = {this.pageSize}  country="in" category="general"/>}>
+            <Route path="/general" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='general' pageSize = {this.pageSize}  country="in" category="general"/>}>
             </Route>
-            <Route path="/health" element={<News setProgress={this.setProgress} key='health' pageSize = {this.pageSize}  country="in" category="health"/>}>
+            <Route path="/health" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='health' pageSize = {this.pageSize}  country="in" category="health"/>}>
             </Route>
-            <Route path="/science" element={<News setProgress={this.setProgress} key='science' pageSize = {this.pageSize}  country="in" category="science"/>}>
+            <Route path="/science" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='science' pageSize = {this.pageSize}  country="in" category="science"/>}>
             </Route>
-            <Route path="/sports" element={<News setProgress={this.setProgress} key='sports' pageSize = {this.pageSize}  country="in" category="sports"/>}>
+            <Route path="/sports" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='sports' pageSize = {this.pageSize}  country="in" category="sports"/>}>
             </Route>
-            <Route path="/technology" element={<News setProgress={this.setProgress} key='technology' pageSize = {this.pageSize}  country="in" category="technology"/>}>
+            <Route path="/technology" element={<News setProgress={this.setProgress} apiKey = {this.apiKey} key='technology' pageSize = {this.pageSize}  country="in" category="technology"/>}>
             </Route>
         </Routes>
         </BrowserRouter>
--- src/Components/News.js
+++ src/Components/News.js
@@ -32,7 +32,7 @@ export class News extends Component {
     }
     async updateNews(){
         this.props.setProgress(10);
-        const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=36f50f2b27604799b41f827583fcd0d0&page=${this.state.page}&pagesize=${this.props.pageSize}`
+        const url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`
         this.setState({loading: true});
         let data = await fetch(url);
         this.props.setProgress(30);
